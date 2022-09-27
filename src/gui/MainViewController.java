dafede84b909b89e47f0b718730f0f15e413561c
Vinicius Rezende <viniciusrocharezende@gmail.com>
2022-09-27 19:11:06 -0300

    View About

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -30,11 +39,31 @@ public class MainViewController implements Initializable{
 
 	@FXML
 	public void onMenuItemAboutACtion() {
-		System.out.println("onMenuItemAboutACtion");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
 
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			//Fazendo a sobrebrosicao da tela principal com uma tela nova mantendo o menu
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);//Pega o primeiro filho do VBOX da janela principal q é o menu
+			mainVBox.getChildren().clear();//Limpa toda a main VBox
+			mainVBox.getChildren().add(mainMenu);//Adiciona o menu
+			mainVBox.getChildren().addAll(newVBox.getChildren());//Adiciona tudo da tela nova na tela antiga
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view",e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
 }
