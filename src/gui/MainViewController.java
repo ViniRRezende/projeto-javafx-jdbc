6dfaf373e3f11b7dffae337de24f8f4bd6d064d5
Vinicius Rezende <viniciusrocharezende@gmail.com>
2022-09-28 18:04:56 -0300

    Acao de inicializacao da tela como parametro no loadView()

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,20 +36,23 @@ public class MainViewController implements Initializable{
 
 	@FXML
 	public void onMenuItemDepartmentACtion() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 		
 	}
 
 	@FXML
 	public void onMenuItemAboutACtion() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
 
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		
 		try {
 			//Fazendo a sobrebrosicao da tela principal com uma tela nova mantendo o menu
@@ -63,29 +67,8 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().add(mainMenu);//Adiciona o menu
 			mainVBox.getChildren().addAll(newVBox.getChildren());//Adiciona tudo da tela nova na tela antiga
 			
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view",e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		
-		try {
-			//Fazendo a sobrebrosicao da tela principal com uma tela nova mantendo o menu
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0);//Pega o primeiro filho do VBOX da janela principal q é o menu
-			mainVBox.getChildren().clear();//Limpa toda a main VBox
-			mainVBox.getChildren().add(mainMenu);//Adiciona o menu
-			mainVBox.getChildren().addAll(newVBox.getChildren());//Adiciona tudo da tela nova na tela antiga
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();//Estas duas linhas vao executar a funcao lambda que passamos como argumento ao chamar o metodo loadView()
+			initializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view",e.getMessage(), AlertType.ERROR);
 		}
